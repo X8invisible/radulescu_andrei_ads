4de7d8490e7782bb18dc222be2f7f0ff65111665
x8invisible <ovidiu.andrey97@gmail.com>
2019-02-21 15:27:28 +0000

    changed display, added players, score

--- main.c
+++ main.c
@@ -4,20 +4,38 @@
 
 #define SIZEOFBD 9
 
+struct History
+{
+    int slotChanged;
+    char sign;
+    struct History *next, *prev;
+};
+struct Player
+{
+    char sign;
+    char name[256];
+    int score;
+};
+
+
+void printScore(struct Player one, struct Player two)
+{
+    printf("\n\nScore x (%s): %d | Score y (%s): %d\n",one.name,one.score,two.name,two.score);
+}
 void printBoard(char *board)
 {
     for(int i = 0; i < SIZEOFBD; i++)
     {
-        printf("%c |", board[i]);
+        printf(" %c ", board[i]);
         if (( i + 1 ) % 3 == 0) {
-           printf("\n");
-        }
+           printf("\n-------------\n");
+        }else
+            printf("|");
         
     }
     
 }
-
-int CheckBoard(char *board)
+char CheckBoard(char *board)
 {
     int index =0;
     for(int x = 0; x < 3; x++)
@@ -25,33 +43,36 @@ int CheckBoard(char *board)
         //horizontal check
         if ((board[index] == board[index + 1]) && (board[index + 1] == board[index +2])) {
             printf("%c wins\n", board[index]);
-            return 1;
+            return board[index];
         }
         //vertical check
         if((board[x] == board[x+3]) && (board[x+6]== board[x+3]))
         {
              printf("%c wins\n", board[x]);
-             return 1;
+             return board[x];
         }
         index += 3;
     }
     if(((board[0] == board[4]) && (board[8] == board[4])) || ((board[2] == board[4]) && (board[6] == board[4])))
     {
          printf("%c wins\n", board[4]);
-             return 1;
+             return board[4];
     }
-    return 0;
+    return 'n';
 }
 
-void Move(char *board, int turn)
+void Change(char *board, struct History *h)
 {
-    char player = 'x';
+    int index = h->slotChanged;
+    char sign = h->sign;
+
+    board[index] = (index+1)+'0';
+}
+void Move(char *board, struct Player p)
+{
+    char player = p.sign;
     int index =10;
     int correct = 0;
-    if (turn % 2 == 1)
-    {
-        player = 'o';
-    }
     do
     {
         printf("Enter a square: ");
@@ -77,16 +98,68 @@ void Move(char *board, int turn)
         
     } while (correct == 0);
 }
+
+void resetGame(char *board)
+{
+    for(int i = 0; i < SIZEOFBD; i++)
+    {
+        board[i] = (i+1) + '0';
+    }
+    
+}
 int main()
 {
-    int turn =0;
-    int gameOver = 0;
-    char board[SIZEOFBD] = {'1','2','3','4','5','6','7','8','9'};
+    struct Player one, two;
+    one.sign = 'x';
+    one.score = 0;
+    two.sign = '0';
+    two.score = 0;
+    printf("Player one name: ");
+    scanf("%s", one.name);
+    printf("Player two name: ");
+    scanf("%s", two.name);
+    int turn =0, gameOver = 0;
+    char winner;
+    char board[SIZEOFBD] = "123456789";
     while(gameOver == 0)
     {
+        struct Player p;
+        if(turn % 2 == 0)
+        {
+            p = one;
+        }else
+        {
+            p = two;
+        }
+        printScore(one,two);
         printBoard(board);
-        Move(&*board, turn);
-        gameOver = CheckBoard(&*board);
+        Move(&*board, p);
+        winner = CheckBoard(&*board);
+        if(winner == 'x')
+        {
+            one.score++;
+        }
+        if (winner =='0') {
+            two.score++;
+        }
+        if(winner != 'n')
+        {
+            printf("Play another?(y/n): ");
+            char choice;
+            scanf(" %c", &choice);
+            if(choice == 'y')
+            {
+                resetGame(&*board);
+            }
+            else
+            {
+                gameOver =1;
+            }
+            
+            
+            
+        }
+        
         turn += 1;
     }
     
